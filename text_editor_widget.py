f8586f45f650a5d882c0b7cbb6347bbcef75668e
ilangovenac <acilangoven@gmail.com>
2021-01-25 10:51:21 +0530

    bold & italic func added

--- text_editor_widget.py
+++ text_editor_widget.py
@@ -1,19 +1,31 @@
 from tkinter import *
-import tkinter.font
+from tkinter import font
 
 root = Tk()
+
 def bold_it():
-
-    bold_font=font.Font(weight='bold')
-    #bold_font=font.Font(font=T['font'])
-    bold_font.configure(weight='bold')
+    bold_font=font.Font(T,T.cget("font"))
+    bold_font.configure(weight="bold")
     
-    T.tag_configure('bold',font=bold_font)
-    T.tag_add('bold', 'sel.first','sel.last')
+    T.tag_configure("bold",font=bold_font)
+    current_tags=T.tag_names("sel.first")
+    
+    if "bold" in current_tags:
+        T.tag_remove("bold","sel.first","sel.last")
+    else:
+        T.tag_add("bold","sel.first","sel.last")
 
-   
-toolbar_frame=Frame(root)
-toolbar_frame.pack(fill=X) 
+def itatics_it():
+    italics_font=font.Font(T,T.cget("font"))
+    italics_font.configure(slant="italic")
+    
+    T.tag_configure("italic",font=italics_font)
+    current_tags=T.tag_names("sel.first")
+    
+    if "italic" in current_tags:
+        T.tag_remove("italic","sel.first","sel.last")
+    else:
+        T.tag_add("italic","sel.first","sel.last")
 
 # specify size of window. 
 root.geometry("350x350")
@@ -26,13 +38,19 @@ scrollbar=Scrollbar(root)
 l=Label(root,text='Trial version',font=('Broadway'))
   
 # Create text widget and specify size. 
-T = Text(root,yscrollcommand=scrollbar.set,height = 10, width = 250,font=('Rockwell'),bg='light blue',fg='dark blue') 
+T = Text(root,yscrollcommand=scrollbar.set,height = 10, width = 250,font=('Rockwell'),bg='light blue') 
  
 # Create an Exit button. 
 b2 = Button(root, text = "Exit",fg='red', command = root.destroy)  
 
-bold_button=Button(toolbar_frame,text='Bold',command=bold_it)
-bold_button.grid(row=0,column=0,sticky=W,padx=5)
+
+#bold button
+bold_button=Button(root,text='Bold',command=bold_it)
+bold_button.pack()
+
+#italics_button
+italics_button=Button(root,text='Italics',command=itatics_it)
+italics_button.pack()
 
 
 scrollbar.pack(side=RIGHT,fill=Y)
